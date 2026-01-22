c7f8f85307b666c53c0ebb3e96e4a56c8ea3441c
valecristiano <valeriamdcristiano@gmail.com>
2026-01-22 17:03:51 +0300

    verifica catch error

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // Elementi da HTML
 
 const emailList = document.getElementById("email-list");
+const errorMessage = document.getElementById("error-message");
 
 //Api
 
-const emailGenerator = "https://flynn.boolean.careers/exercises/api/random/mail";
+const emailGenerator = "https://flynn.boolean.careers/exercises/api/random/mails";
 
 for (let i = 0; i < 10; i++) {
-  axios.get(emailGenerator).then((response) => {
-    emailList.innerHTML += `<li>${response.data.response}</li>`;
-  });
+  axios
+    .get(emailGenerator)
+    .then((response) => {
+      emailList.innerHTML += `<li>${response.data.response}</li>`;
+    })
+    .catch((error) => {
+      errorMessage.classList.remove("d-none");
+    })
+    .finally(() => {});
 }
