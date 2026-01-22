96a0379f1b915db653198ba2c1cabc61f326e5bc
valecristiano <valeriamdcristiano@gmail.com>
2026-01-22 16:57:07 +0300

    email list generation

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("ciao");
+// Elementi da HTML
+
+const emailList = document.getElementById("email-list");
+
+//Api
+
+const emailGenerator = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+for (let i = 0; i < 10; i++) {
+  axios.get(emailGenerator).then((response) => {
+    emailList.innerHTML += `<li>${response.data.response}</li>`;
+  });
+}
