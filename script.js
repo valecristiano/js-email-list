d17b602085cbec8dcaeb677e0dee06393a3ae763
valecristiano <valeriamdcristiano@gmail.com>
2026-01-22 18:10:22 +0300

    final commit

--- script.js
+++ script.js
@@ -9,30 +9,39 @@ const newEmailButton = document.getElementById("new-emails-btn");
 
 const emailGenerator = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-function emailGeneration(numeroEmail) {
-  emailList.innerHTML = "";
-  let c = 0;
-
-  for (let i = 0; i < numeroEmail; i++) {
-    axios
-      .get(emailGenerator)
-      .then((response) => {
-        emailList.innerHTML += `<li>${response.data.response}</li>`;
-      })
-      .catch((error) => {
-        errorMessage.classList.remove("d-none");
-      })
-      .finally(() => {
-        c++;
-        if (c >= 10) {
-          finallyMessage.classList.remove("d-none");
-        }
-      });
-  }
-}
+//Generazione email
 
 emailGeneration(10);
 
 newEmailButton.addEventListener("click", () => {
   emailGeneration(10);
 });
+
+//Funzioni
+
+function emailGeneration(numeroEmail) {
+  emailList.innerHTML = "";
+  finallyMessage.classList.add("d-none");
+
+  let c = 0;
+
+  for (let i = 0; i < numeroEmail; i++) {
+    axios
+      .get(emailGenerator)
+      .then((response) => {
+        emailList.innerHTML += `<li class="list-group-item">${response.data.response}</li>`;
+      })
+      .catch((error) => {
+        errorMessage.classList.remove("d-none");
+        newEmailButton.addEventListener("click", () => {
+          emailGeneration(10);
+        });
+      })
+      .finally(() => {
+        c++;
+        if (c >= numeroEmail) {
+          finallyMessage.classList.remove("d-none");
+        }
+      });
+  }
+}
